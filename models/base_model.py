1e48685bfc4c154833a64190c3cc86d97ba0109a
sayedali1 <said.ali99859@gmail.com>
2023-07-11 02:47:56 +0300

    add init and str fun ,init id and create_at

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,47 @@
 #!/usr/bin/python3
-"""This module (called BaseModel) to take care of the initialization,
- serialization and deserialization of your future instances"""
+import uuid
+from datetime import datetime
+
+"""
+This module (called BaseModel) to take care of the initialization,
+serialization and deserialization of your future instances
+"""
+
 
 class BaseModel:
-    """ this class defines all common attributes/methods for other classes"""
+    """ 
+    this class defines all common attributes/methods for other classes
+    
+    Attributes:
+        id (str) : public ,generate unique id
+        create_at : assign with the current datetime
+    """
+    
+    id = str(uuid.uuid4())
+    create_at = datetime.now()
+
+    def __init__(self, my_number=None, name=None,):
+        """
+        initialition
+        Attribures:
+            my_number: model number
+            name: model name
+            id: model id
+        """
+        self.my_number = my_number
+        self.name = name
+        self.id = self.__class__.id
+        
+    def __str__(self):
+        """ string format of the model """
+        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+""" if __name__ == '__main__':
+    from base_model import BaseModel
+    
+    model = BaseModel()
+    model.name = "sayed"
+    model.my_number = 9
+    print(model)
+   
+     """
