6d91c8698c5d9333626787baa845614823b896a7
Mahmoud Elbehery <mahmoudramy84@gmail.com>
2023-07-11 00:03:56 +0300

    Adding models/base_model.py, models/__init__.py tasks file

---
+++ models/base_model.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""This module (called BaseModel) to take care of the initialization,
+ serialization and deserialization of your future instances"""
+
+class BaseModel:
+    """ this class defines all common attributes/methods for other classes"""
