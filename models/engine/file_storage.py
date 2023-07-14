b6801368269661eb43c18274e42c484900ccb71a
Mahmoud Elbehery <mahmoudramy84@gmail.com>
2023-07-14 18:10:16 +0300

    Update FileStorage to manage serialization and deserialization of User.

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import json
 from models.base_model import BaseModel
+from models.user import User
 
 """ 
 model to covert dict to a json and store in file
@@ -17,7 +18,11 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-    class_dict = {"BaseModel": BaseModel}
+    class_dict = {
+        "BaseModel": BaseModel,
+        "User": User,
+    }
+
     def all(self):
         """ returns the dictionary __objects """
         return self.__objects
