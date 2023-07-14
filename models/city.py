903a4b09ddcd9ca41ae87bf728189fe254b4775a
Mahmoud Elbehery <mahmoudramy84@gmail.com>
2023-07-14 18:00:58 +0300

    added city.py place.py classes files

---
+++ models/city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+from models.base_model import BaseModel
+
+""" class City """
+class City(BaseModel):
+    """ 
+    Attributes:
+    state_id : it will be the State.id
+    name:
+    """
+    state_id = ""
+    name = ""
