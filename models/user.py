b1f56e70198d0852e75b220f883f45f4b7268ccf
sayedali1 <said.ali99859@gmail.com>
2023-07-14 09:29:31 +0300

    class user

---
+++ models/user.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+from models.base_model import BaseModel
+
+""" class user """
+class User(BaseModel):
+    """ 
+    Attributes:
+    email
+    password
+    first_name
+    last_name
+    """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
