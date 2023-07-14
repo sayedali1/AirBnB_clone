3a700d6bfd7dd457e83d807a2d195533b4f055be
Mahmoud Elbehery <mahmoudramy84@gmail.com>
2023-07-14 18:02:59 +0300

    Updated file console.py and adding show& all functions

--- console.py
+++ console.py
@@ -5,7 +5,7 @@ from models.base_model import BaseModel
 from models import storage
 class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
-    
+  
     def do_create(self, line):
         if line == "":
             print("** class name missing **")
@@ -16,13 +16,33 @@ class HBNBCommand(cmd.Cmd):
             new_instance.save()
             print(new_instance.id)
 
-    def do_delete(self, line):
+    def do_show(self, line):
+        """Prints the string representation of an instance
+        based on the class name and id"""
+        line_args = line.split()
+        if not line_args:
+            print("** class name missing **")
+            return
+        try:
+            class_name = line_args[0]
+            obj_id = line_args[1]
+            obj = storage.all().get(f"{class_name}.{obj_id}")
+            if obj:
+                print(obj)
+            else:
+                print("** no instance found **")
+        except IndexError:
+            print("** instance id missing **")
+        except NameError:
+            print("** class doesn't exist **")    
+    
+    def do_destroy(self, line):
         line_args = line.split(" ")
         if line_args[0] == "":
             print("** class name missing **")
-        elif line[0] != "BaseModel":
+        elif line_args[0] != "BaseModel":
             print("** class doesn't exist **")
-        elif line[1] == "":
+        elif line_args[1] == "":
             print("** instance id missing **")
         key = "{}.{}".format(line[0], line[1]) 
         if  key not in storage.all().keys():
@@ -30,7 +50,20 @@ class HBNBCommand(cmd.Cmd):
         else:
             del storage.all()[key]
             storage.save()
-    
+
+    def do_all(self, line):
+        """Prints all string representation of all instances
+        based or not on the class name"""
+        objects = storage.all()
+        if not line:
+            print([str(obj) for obj in objects.values()])
+        else:
+            try:
+                class_name = line
+                print([str(obj) for obj in objects.values() if obj.__class__.__name__ == class_name])
+            except NameError:
+                print("** class doesn't exist **")
+ 
     def do_update(self, line):
         """Update if given exact object, exact attribute"""
         args = line.split(" ")
@@ -56,7 +89,7 @@ class HBNBCommand(cmd.Cmd):
             print("** value missing **")
 
             
-            
+           
 
     def emptyline(self):
         """ override the empty line """
@@ -70,7 +103,7 @@ class HBNBCommand(cmd.Cmd):
         """ exit the program when we press contrl-D """
         print("\n")
         return True
-    
-        
+   
+       
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
